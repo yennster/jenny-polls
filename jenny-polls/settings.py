1c35ffca52006c905aca618847d2ca576962b0f6
Jenny Plunkett <jplunkett@utexas.edu>
2017-04-02 22:30:00 -0500

    added databases var

--- jenny-polls/settings.py
+++ jenny-polls/settings.py
@@ -37,6 +37,7 @@ STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 # Update database configuration with $DATABASE_URL.
 import dj_database_url
 db_from_env = dj_database_url.config()
+DATABASES = { 'default': dj_database_url.config() }
 DATABASES['default'].update(db_from_env)
 
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
