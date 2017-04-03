3204e4ef53064bf55e8785d7e14f9951281de730
Jenny Plunkett <jplunkett@utexas.edu>
2017-04-02 22:25:20 -0500

    changed settings

--- jenny-polls/settings.py
+++ jenny-polls/settings.py
@@ -30,17 +30,16 @@ ALLOWED_HOSTS = []
 TIME_ZONE = 'America/Chicago'
 
 # Update database configuration with $DATABASE_URL.
-#import dj_database_url
-#db_from_env = dj_database_url.config()
-#DATABASES['default'].update(db_from_env)
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.9/howto/static-files/
+import dj_database_url
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
 
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
-STATIC_URL = '/static/'
+STATIC_URL = '/polls/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
