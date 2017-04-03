7fcea65ff1f4533b4438331e62508c70629cbfbe
Jenny Plunkett <jplunkett@utexas.edu>
2017-04-02 21:51:15 -0500

    Added heroku files

--- jenny-polls/wsgi.py
+++ jenny-polls/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for hellowebapp project.
+WSGI config for jenny-polls project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hellowebapp.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "jenny-polls.settings")
 
 application = get_wsgi_application()
